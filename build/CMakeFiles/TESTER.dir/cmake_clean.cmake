2af7ae6135d4f62a3d24a3f1853ec27be25b922d
AnthonyMesa <anthony.mesa.propst@gmail.com>
2019-10-24 15:00:06 -0400

    test

---
+++ build/CMakeFiles/TESTER.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TESTER.dir/To_bpp.cpp.o"
+  "TESTER"
+  "TESTER.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TESTER.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
