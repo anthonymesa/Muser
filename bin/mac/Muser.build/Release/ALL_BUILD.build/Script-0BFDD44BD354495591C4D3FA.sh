41b16aeba0d4cc57b42f039a4f67b510e86bbad7
Anthony David Mesa <ADMESA@campus.uncg.edu>
2019-11-07 09:17:49 -0500

    delete

--- bin/mac/Muser.build/Release/ALL_BUILD.build/Script-0BFDD44BD354495591C4D3FA.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/anthonymesa/Desktop/muser-build/bin/mac -f /Users/anthonymesa/Desktop/muser-build/bin/mac/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION OBJDIR=$(basename "$OBJECT_FILE_DIR_normal") all
--- bin/mac/Muser.build/Release/Muser.build/Objects-normal/x86_64/RenderMuser.d
+++
@@ -1,2 +0,0 @@
-dependencies: /Users/anthonymesa/Desktop/muser-build/src/RenderMuser.cpp \
-  /Users/anthonymesa/Desktop/muser-build/src/header/../header/RenderMuser.h
